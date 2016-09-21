1e04d274e40a58d85ebc3f608c374b6090c94eee
Roman Chuprikov <paradise.develop@gmail.com>
2016-09-21 19:22:50 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const BN = require('bn.js')
+const SHA3 = require('keccakjs')
+const secp256k1 = require('secp256k1')
+
+var sha3bits = 256;
+
+/**
+ * Signs hash 'string' with private key 'string' returns 'string' signed hash (hex)
+ * @method signHash
+ * @param {Buffer} hash
+ * @param {Buffer} privateKey
+ * @return {String}
+ */
+exports.signHash = function (hash, privateKey) {
+    var bufferHash = new Buffer(padToEven(stripHexPrefix(hash)), 'hex'),
+        bufferPrivateKey = new Buffer(padToEven(stripHexPrefix(privateKey)), 'hex');
+
+    var sig = secp256k1.sign(bufferHash, bufferPrivateKey);
+    var result = Buffer.concat([sig.signature, intToBuffer(sig.recovery)])
+    return '0x' + result.toString('hex');
+}
+
+/**
+* Calculates hash from input arguments returns 'string' hex
+* @param {...*} var_args (hex string or number)
+* @return {String}
+*/
+exports.getHash = function (var_args) {
+    var args = arguments,
+        buffer = [];
+
+    for (var i = 0; i < args.length; i++) {
+        var item = args[i];
+        if (typeof item === 'string') {
+            buffer.push(new Buffer(padToEven(stripHexPrefix(item)), 'hex'));
+        }
+        else if (typeof item === 'number') {
+            buffer.push(new BN(item, 10).toBuffer('be', 32));
+        }
+        else {
+            throw new Error('invalid type')
+        }
+    }
+    var hash = new SHA3(sha3bits).update(Buffer.concat(buffer)); 
+    return hash.digest('hex');
+}
+
+function isHexPrefixed(str) {
+    return str.slice(0, 2) === '0x';
+}
+
+function padToEven(a) {
+    if (a.length % 2) a = '0' + a;
+    return a;
+}
+
+function stripHexPrefix(str) {
+    if (typeof str !== 'string') {
+        return str;
+    }
+    return isHexPrefixed(str) ? str.slice(2) : str;
+}
+
+function isValidPrivate(privateKey) {
+    return secp256k1.privateKeyVerify(privateKey);
+}
+
+function intToBuffer(i) {
+    var hex = intToHex(i);
+    return new Buffer(hex.slice(2), 'hex');
+}
+    
+    nction intToHex (i) {
+        r hex = i.toString(16);
+    if (hex.length % 2) {
+      hex = '0' + hex;
+  }
+  return '0x' + hex;
+}
---
+++ test/index.js
@@ -0,0 +1,40 @@
+var assert = require('assert')
+var lykkeEth = require('../index.js')
+const BN = require('bn.js')
+const SHA3 = require('keccakjs')
+
+var account_a = "0x960336a077fB32d675405bd0A6cD0cb74aaa5062",
+    account_b = "0xb295e245eD2fdf5776c3C8a49f0403BF0242262A",
+    private_key_a = "4085dde01ea641a0f4fd6586ca11fc1f5df38e1bdcbef501da970cad9335b389",
+    private_key_b = "74ed04f45c2a375a94189ef69661fa08235bb3b76be65934a0827262542e870c",
+    amount_a = 10;
+
+describe('check hash', function () {
+  it('hash from adress', function () {
+    var result = "c432894b6d988881dcc107a658b0b7f7334e5410bc422ef4654fd382070b9c5a";
+    assert.equal(result, lykkeEth.getHash(account_a));
+  })
+
+  it('hash from adress + addre ss', function () {
+    var result = "0706790bb1d27247d755f75904f842a50142d5fd0318052fdf51dc1885e46f97";
+    assert.equal(result, lykkeEth.getHash(account_a, account_b));
+  })
+
+  it('hash from number', function () {
+    var result = "c65a7bb8d6351c1cf70c95a316cc6a92839c986682d98bc35f958f4883f9d2a8";
+    assert.equal(result, lykkeEth.getHash(amount_a));
+  })
+
+  it('hash from address + address + number', function () {
+    var result = "0d95bd996b826a43630082d99723ae1fb5d24c9077551a36e3c07ddc8e1b8701";
+    assert.equal(result, lykkeEth.getHash(account_a, account_b, amount_a));
+  })
+})
+
+describe('check hash', function () {
+  it('check signature', function () {
+    var sig = lykkeEth.signHash(lykkeEth.getHash(amount_a), private_key_a);
+    console.log(sig);
+  })
+})
+
